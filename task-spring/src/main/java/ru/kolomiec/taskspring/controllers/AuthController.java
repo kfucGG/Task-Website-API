83b5fc72bcb92f2b1dbaaacfa8403f840c119c14
kfucGG <zaharnagibator1337@gmail.com>
2023-01-24 15:54:20 +0300

    added description on authcontroller and request  matchers in security config for ignorring swagger-ui

--- task-spring/src/main/java/ru/kolomiec/taskspring/controllers/AuthController.java
+++ task-spring/src/main/java/ru/kolomiec/taskspring/controllers/AuthController.java
@@ -1,30 +1,23 @@
 package ru.kolomiec.taskspring.controllers;
 
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 import ru.kolomiec.taskspring.entity.Person;
-import ru.kolomiec.taskspring.repository.PersonRepository;
 import ru.kolomiec.taskspring.security.jwt.JwtRequest;
 import ru.kolomiec.taskspring.security.jwt.JwtResponse;
 import ru.kolomiec.taskspring.security.jwt.JwtUtil;
-import ru.kolomiec.taskspring.services.PersonServiceImpl;
 import ru.kolomiec.taskspring.services.interfaces.PersonService;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication controller", description = "authentication controller with jwt token")
 public class AuthController {
 
     private final JwtUtil jwtUtil;
@@ -32,12 +25,14 @@ public class AuthController {
 
     //todo add exception handler for badCredentials
     @PostMapping("/login")
+    @Operation(summary = "login with username and password and returnes jwt token")
     public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest jwtRequest) {
         personService.isProcessAuthPersonPrincipalIsValid(jwtRequest);
         return ResponseEntity.ok(jwtUtil.generateToken(jwtRequest.getUsername()));
     }
 
     @PostMapping("/registration")//todo maybe should create entity class for registration(DTO maybe)
+    @Operation(summary = "registration new person and returns jwt token of this person")
     public ResponseEntity<JwtResponse> registration(@RequestBody Person person) {
         personService.savePerson(person);//todo add validation and exception handlers (ControllerAdvice.class)
         return ResponseEntity.ok(jwtUtil.generateToken(person.getUsername()));
--- task-spring/src/main/java/ru/kolomiec/taskspring/security/SecurityConfiguration.java
+++ task-spring/src/main/java/ru/kolomiec/taskspring/security/SecurityConfiguration.java
@@ -10,10 +10,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEditor;
 import ru.kolomiec.taskspring.security.jwt.JwtFilter;
 import ru.kolomiec.taskspring.services.PersonDetailsService;
 
@@ -22,19 +25,32 @@ import ru.kolomiec.taskspring.services.PersonDetailsService;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    //For swagger-ui without authentication
+    private static final String[] AUTH_WHITELIST = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
 
+    //todo change all controllers should starts with /api
     private final JwtFilter jwtFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
         http.authorizeRequests(registry -> {
            registry.requestMatchers("/auth/*").permitAll();
+           registry.requestMatchers(AUTH_WHITELIST).permitAll();
            registry.anyRequest().authenticated();
         });
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
     //todo make normal password encoder
     @Bean
     public PasswordEncoder passwordEncoder() {
