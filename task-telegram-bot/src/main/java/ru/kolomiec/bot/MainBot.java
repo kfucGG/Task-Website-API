2e097d11efd84d9c0836697837b16f77ce400083
kfucGG <zaharnagibator1337@gmail.com>
2023-02-15 13:14:07 +0300

    added class for creating task(TaskCreateSession)

--- task-telegram-bot/src/main/java/ru/kolomiec/bot/MainBot.java
+++ task-telegram-bot/src/main/java/ru/kolomiec/bot/MainBot.java
@@ -1,7 +1,6 @@
 package ru.kolomiec.bot;
 
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -15,20 +14,25 @@ import ru.kolomiec.database.dao.PersonDAO;
 import ru.kolomiec.dto.TaskDTO;
 import ru.kolomiec.service.TaskService;
 
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
-import java.util.logging.Logger;
 
 
 public class MainBot extends TelegramLongPollingCommandBot {
 
-    private final String key = "6116678801:AAEzHaMCHlMbyF56KSTvL5PW8oiIYAdmN78";
+    private final String KEY = "6116678801:AAEzHaMCHlMbyF56KSTvL5PW8oiIYAdmN78";
+    private final String BOT_NAME = "Task385Bot";
     private final PersonDAO personDAO = new PersonDAO();
-
     private final TaskService taskService = new TaskService();
+    private TaskCreateSession taskCreateSession = null;
+    private boolean isNextMessageIsTask = false;
     public MainBot() {
         register(new StartCommand("/start", "start command"));
         register(new HelpCommand("/help", "shows all commands"));
@@ -37,50 +41,38 @@ public class MainBot extends TelegramLongPollingCommandBot {
     }
     @Override
     public String getBotUsername() {
-        return "Task385Bot";
+        return BOT_NAME;
     }
 
     public String getBotToken() {
-        return key;
+        return KEY;
     }
 
-    private boolean isNextMessageIsTask = false;
 
-    private boolean today = false;
 
-    private boolean anotherDay = false;
-
-    private boolean noTime = false;
-
-    private TaskDTO task = null;
-
-    Logger log = Logger.getLogger(this.getClass().getName());
     @Override
+    @SneakyThrows
     public void processNonCommandUpdate(Update update)  {
         System.out.println("in non proccess method");
         Message message = update.getMessage();
         Long chatId = message.getChatId();
         if (!personDAO.isPersonSavedInDb(chatId)) {
             System.out.println("person does not exists");
-            try {
-                execute(new SendMessage().builder().chatId(chatId).text("you are not auth").build());
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
+            execute(new SendMessage().builder().chatId(chatId).text("you are not auth").build());
             return;
         }
 
         if (update.getMessage().getText().equals("all tasks")) {
             System.out.println("person is want to get all tasks");
             TaskDTO[] tasks = taskService.getAllTasksFromApi(chatId);
-            task = null;
+            taskCreateSession = null;
             sendKeyboard(update,"all your task: %s".formatted(Arrays.toString(tasks)), getMainKeyboard());
             return;
         }
 
         if (update.getMessage().getText().equals("new task")) {
             System.out.println("person is decided to create new task");
-            task = new TaskDTO();
+            taskCreateSession = new TaskCreateSession();
             isNextMessageIsTask = true;
             try {
                 execute(new SendMessage().builder().chatId(chatId).text("enter task name").build());
@@ -90,74 +82,63 @@ public class MainBot extends TelegramLongPollingCommandBot {
             return;
         }
 
-        if (message.getText().equals("another day")) {
-            System.out.println("message is another day");
-            anotherDay = true;
-            try {
-                execute(SendMessage
-                        .builder()
-                        .chatId(chatId)
-                        .text("input date in format yyyy-MM-ddTHH:mm:ss").build());
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
-            return;
-        }
-
-        if (message.getText().equals("today")) {
-            System.out.println("message is today");
-            today = true;
-            try {
-                execute(SendMessage
-                        .builder()
-                        .chatId(chatId)
-                        .text("Input time in format HH:mm or HH:mm:ss").build());
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
-            return;
-        }
-
         if (isNextMessageIsTask) {
             System.out.println("user input a task name");
             String text = update.getMessage().getText();
-            task.setTaskName(text);
+            taskCreateSession.setTaskDTO(new TaskDTO(text, null));
             isNextMessageIsTask = false;
             sendKeyboard(update, "When task should be done?", getChooseTaskTimeKeyboard());
             return;
         }
 
-        if (message.getText().equals("no time")) {
-            System.out.println("message is no time");
-            taskService.saveNewTaskToApi(chatId, task);
-            sendKeyboard(update, "task is saved without time", getMainKeyboard());
-            task = null;
+        if (message.getText().equals("another day")) {
+            System.out.println("message is another day");
+            taskCreateSession.setTimeToDo(TimeToDo.ANOTHER_DAY);
+            execute(SendMessage.builder().chatId(chatId).text("input date in format yyyy-MM-ddTHH:mm:ss").build());
             return;
         }
 
-        if (anotherDay) {
+        if (message.getText().equals("today")) {
+            System.out.println("message is today");
+            taskCreateSession.setTimeToDo(TimeToDo.TODAY);
+            execute(SendMessage
+                    .builder()
+                    .chatId(chatId)
+                    .text("Input time in format HH:mm or HH:mm:ss").build());
+            return;
+        }
+
+        if (message.getText().equals("no time")) {
+            System.out.println("message is no time");
+            taskService.saveNewTaskToApi(chatId, taskCreateSession.getTaskDTO());
+            sendKeyboard(update, "task is saved without time", getMainKeyboard());
+            taskCreateSession = null;
+            return;
+        }
+
+        if (taskCreateSession.getTimeToDo().compareTo(TimeToDo.ANOTHER_DAY) == 0) {
             System.out.println("another day");
-            anotherDay = false;
-            System.out.println(message.getText());
-            task.setToDoTime(LocalDateTime.parse(message.getText(), DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-            taskService.saveNewTaskToApi(chatId, task);
-            task = null;
+            LocalDateTime taskTime = LocalDateTime.parse(message.getText(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            taskCreateSession.setTaskDTO(new TaskDTO(taskCreateSession.getTaskDTO().getTaskName(), taskTime));
+            taskService.saveNewTaskToApi(chatId, taskCreateSession.getTaskDTO());
+            taskCreateSession = null;
             sendKeyboard(update, "Task is saved", getMainKeyboard());
             return;
         }
 
-        if (today) {
+        if (taskCreateSession.getTimeToDo().compareTo(TimeToDo.TODAY) == 0) {
             System.out.println("today");
-            var tt = LocalDateTime.parse(message.getText(), DateTimeFormatter.ISO_LOCAL_TIME);
-            task.setToDoTime(tt);
-            taskService.saveNewTaskToApi(chatId, task);
-            task = null;
+            System.out.println("message is" + message.getText());
+            var time = LocalTime.parse(message.getText());
+            System.out.println("time is" + time);
+            taskCreateSession.setTaskDTO(new TaskDTO(taskCreateSession.getTaskDTO().getTaskName(), time.atDate(LocalDate.now())));
+            taskService.saveNewTaskToApi(chatId, taskCreateSession.getTaskDTO());
+            taskCreateSession = null;
+            sendKeyboard(update, "Task is saved", getMainKeyboard());
             return;
         }
         sendKeyboard(update, "Choose option", getMainKeyboard());
-        log.info("end of method");
     }
-
     private void sendKeyboard(Update update, String text, ReplyKeyboard keyBoard) {
         try {
             execute(new SendMessage().builder().text(text).chatId(update
